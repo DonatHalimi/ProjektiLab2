cfe10e24f14742ce188d7551dccd4a5acff2cfe3
VullnetLipovica <vl57425@ubt-uni.net>
2024-12-11 15:05:12 +0100

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="container flex h-screen justify-center items-center">
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello world!!!!!
       </h1>
     </div>
   )
